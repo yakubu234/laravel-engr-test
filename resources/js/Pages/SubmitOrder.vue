fd1859bfaa6e0ab0f0cc5f839a13a965847dd564
curacelDev <curaceldev@gmail.com>
2024-07-18 15:01:55 +0100

    replace root route with submit order dummy page

---
+++ resources/js/Pages/SubmitOrder.vue
@@ -0,0 +1,19 @@
+<template>
+    <GuestLayout>
+        <Head title="Register" />
+
+        <div class="card-header">Submit An Order</div>
+
+        <div class="card-body">
+            Order details here
+        </div>
+    </GuestLayout>
+</template>
+
+<script setup>
+import {Head} from "@inertiajs/vue3";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
+console.log('submit order page loaded')
+
+</script>
